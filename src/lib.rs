e9b6fea52a63d46fef2bff7b812dc601ef8f24fa
Nicolas Guiard <nicolas.nguiard@gmail.com>
2024-01-15 15:15:13 +0100

    fix: naming problems

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub use from_pg_fit_row_impl::*;
 
 /// Beware, check your SQL for correctness! Fields should be in the exact correct order 
 pub trait FromPgFitRow {
-    fn from_fit_pg_row(row: Row) -> Self;
+    fn from_pg_fit_row(row: Row) -> Self;
 }
 
 #[cfg(test)]
